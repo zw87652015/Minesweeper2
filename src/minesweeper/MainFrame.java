cc112262be10d4c89b63f6ac2034dbdf1e241479
Meow <zw87652016@gmail.com>
2021-05-20 18:17:16 +0800

    丑化了启动界面

--- src/minesweeper/MainFrame.java
+++ src/minesweeper/MainFrame.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
@@ -27,6 +29,7 @@ public class MainFrame extends JFrame {
     public static int findedMine=0;
     public static int clickNum=0;
     public static  int stepCount=2;
+    private int level=1;//1, 2, 3, 4 for basic, intermediate, advanced and customized, respectively.
 
     private JPanel jp;
     private JTextField selectLevel, selectNumberOfPlayers;
@@ -35,7 +38,10 @@ public class MainFrame extends JFrame {
     private JRadioButton basic, intermediate, advanced, customized, twoPlayers, customizedPlayers;
     private JButton startButton, loadButton, OK;
 
-    private int level=1;//1, 2, 3, 4 for basic, intermediate, advanced and customized, respectively.
+    ImageIcon OKImage = new ImageIcon("E:\\Java\\Java Codes\\Project MineSweeper\\Minesweeper2\\src\\minesweeper\\Materials\\OK.png");
+    ImageIcon StartImage = new ImageIcon("E:\\Java\\Java Codes\\Project MineSweeper\\Minesweeper2\\src\\minesweeper\\Materials\\Start.png");
+    ImageIcon LoadImage = new ImageIcon("E:\\Java\\Java Codes\\Project MineSweeper\\Minesweeper2\\src\\minesweeper\\Materials\\Load.png");
+
 
     private class RadioButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent ae) {
@@ -43,6 +49,9 @@ public class MainFrame extends JFrame {
                 customizedX.setEditable(false);
                 customizedY.setEditable(false);
                 customizedMineCount.setEditable(false);
+                customizedX.setText("9");
+                customizedY.setText("9");
+                customizedMineCount.setText("10");
                 startButton.setVisible(false);
                 xCount=9;yCount=9;mineCount=10;level=1;
             }
@@ -50,6 +59,9 @@ public class MainFrame extends JFrame {
                 customizedX.setEditable(false);
                 customizedY.setEditable(false);
                 customizedMineCount.setEditable(false);
+                customizedX.setText("16");
+                customizedY.setText("16");
+                customizedMineCount.setText("40");
                 startButton.setVisible(false);
                 xCount=16;yCount=16;mineCount=40;level=2;
             }
@@ -57,6 +69,9 @@ public class MainFrame extends JFrame {
                 customizedX.setEditable(false);
                 customizedY.setEditable(false);
                 customizedMineCount.setEditable(false);
+                customizedX.setText("16");
+                customizedY.setText("30");
+                customizedMineCount.setText("99");
                 startButton.setVisible(false);
                 xCount=16;yCount=30;mineCount=99;level=3;
             }
@@ -90,7 +105,7 @@ public class MainFrame extends JFrame {
                         acquiredData.setText("The board is too large or mines are too many!");
                     } else {
                         startButton.setVisible(true);
-                        acquiredData.setText("X:"+String.valueOf(xCount)+" Y:" + String.valueOf(yCount) + " Mine:" + String.valueOf(mineCount) + " Players:" + String.valueOf(numberOfPlayers));
+                        acquiredData.setText("将以 X:"+String.valueOf(xCount)+" Y:" + String.valueOf(yCount) + " Mine:" + String.valueOf(mineCount) + " Players:" + String.valueOf(numberOfPlayers) + "开始游戏");
                     }
                 } catch (NumberFormatException e) {
                     acquiredData.setText("Please input numbers only!");
@@ -165,6 +180,7 @@ public class MainFrame extends JFrame {
 
     public MainFrame(String frameName) {
         super("Mine Sweeper");
+        setBackground(new Color(226,226,226));
 
         this.xCount=9;
         this.yCount=9;
@@ -172,60 +188,92 @@ public class MainFrame extends JFrame {
         this.numberOfPlayers=2;
 
         RadioButtonListener listener = new RadioButtonListener();
-        setSize(300,500);
+        setSize(500,400);
         this.setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 
-        selectLevel = new JTextField("Please select game difficulty");
+        selectLevel = new JTextField("难度");
         selectLevel.setEditable(false);
         selectLevel.setHorizontalAlignment(JTextField.CENTER);
         selectLevel.setBounds(0,0,10,5);
+        selectLevel.setBorder(new EmptyBorder(0,0,0,0));
+        selectLevel.setForeground(new Color(66,65,66));
+        selectLevel.setFont(new Font("叶根友毛笔行书简体-个人版",Font.PLAIN,30));
         levelGroup = new ButtonGroup();
-        basic = new JRadioButton("Basic", true);
-        basic.setBounds(10,0,10,5);
-        intermediate = new JRadioButton("Intermediate");
+        basic = new JRadioButton("初级", true);
+        basic.setBounds(0,10,10,5);
+        basic.setBorder(new EmptyBorder(0,0,0,0));
+        basic.setHorizontalAlignment(JRadioButton.CENTER);
+        intermediate = new JRadioButton("中级");
         intermediate.setBounds(10,10,10,5);
-        advanced = new JRadioButton("Advances");
+        intermediate.setBorder(new EmptyBorder(0,0,0,0));
+        intermediate.setHorizontalAlignment(JRadioButton.CENTER);
+        advanced = new JRadioButton("高级");
         advanced.setBounds(10,20,10,5);
-        customized = new JRadioButton("Customized");
+        advanced.setBorder(new EmptyBorder(0,0,0,0));
+        advanced.setHorizontalAlignment(JRadioButton.CENTER);
+        customized = new JRadioButton("自定义");
         customized.setBounds(10,30,10,5);
-        promptX = new JTextField("Please enter the length (number of blocks)");
+        customized.setBorder(new EmptyBorder(0,0,0,0));
+        customized.setHorizontalAlignment(JRadioButton.CENTER);
+        promptX = new JTextField("棋盘长度");
         promptX.setEditable(false);
         promptX.setBounds(10,40,5,5);
+        promptX.setBorder(new EmptyBorder(0,0,0,0));
+        promptX.setHorizontalAlignment(JTextField.CENTER);
         customizedX = new JTextField("9");
         customizedX.setEditable(false);
         customizedX.setBounds(20,40,10,5);
-        promptY = new JTextField("Please enter the height (number of blocks)");
+        customizedX.setBorder(new EmptyBorder(0,0,0,0));
+        promptY = new JTextField("棋盘宽度");
         promptY.setEditable(false);
         promptY.setBounds(10,50,5,5);
+        promptY.setBorder(new EmptyBorder(0,0,0,0));
+        promptY.setHorizontalAlignment(JTextField.CENTER);
         customizedY = new JTextField("9");
         customizedY.setEditable(false);
         customizedY.setBounds(20,50,10,5);
-        promtMineCount= new JTextField("Please enter the number of mines");
+        customizedY.setBorder(new EmptyBorder(0,0,0,0));
+        promtMineCount= new JTextField("雷数");
         promtMineCount.setEditable(false);
         promtMineCount.setBounds(10,60,5,5);
+        promtMineCount.setBorder(new EmptyBorder(0,0,0,0));
+        promtMineCount.setHorizontalAlignment(JTextField.CENTER);
         customizedMineCount = new JTextField("10");
         customizedMineCount.setEditable(false);
         customizedMineCount.setBounds(20,60,10,5);
+        customizedMineCount.setBorder(new EmptyBorder(0,0,0,0));
+        /*
         promptNumberOfPlayers= new JTextField("Please enter the number of players");
         promptNumberOfPlayers.setEditable(false);
         promptNumberOfPlayers.setBounds(10,70,5,5);
+        */
 
-        selectNumberOfPlayers = new JTextField("Please select number of players");
+        selectNumberOfPlayers = new JTextField("玩家数");
         selectNumberOfPlayers.setEditable(false);
         selectNumberOfPlayers.setHorizontalAlignment(JTextField.CENTER);
         selectNumberOfPlayers.setBounds(0,80,10,5);
+        selectNumberOfPlayers.setBorder(new EmptyBorder(0,0,0,0));
+        selectNumberOfPlayers.setHorizontalAlignment(JTextField.CENTER);
+        selectNumberOfPlayers.setForeground(new Color(66,65,66));
+        selectNumberOfPlayers.setFont(new Font("叶根友毛笔行书简体-个人版",Font.PLAIN,30));
         playersGroup = new ButtonGroup();
-        twoPlayers = new JRadioButton("2 Players", true);
-        customizedPlayers = new JRadioButton("Customized number of players");
+        twoPlayers = new JRadioButton("双人", true);
+        twoPlayers.setHorizontalAlignment(JRadioButton.CENTER);
+        customizedPlayers = new JRadioButton("自定义玩家数");
+        customizedPlayers.setBorder(new EmptyBorder(0,0,0,0));
+        customizedPlayers.setHorizontalAlignment(JRadioButton.CENTER);
         customizedNumberOfPlayers = new JTextField("2");
         customizedNumberOfPlayers.setEditable(false);
         customizedNumberOfPlayers.setBounds(10,90,10,5);
+        customizedNumberOfPlayers.setBorder(new EmptyBorder(0,0,0,0));
 
-        acquiredData = new JTextField("X:9 Y:9 Mine:10 Players:2");
+        acquiredData = new JTextField("将以 X:9 Y:9 Mine:10 Players:2 开始游戏");
         acquiredData.setEditable(false);
         acquiredData.setBounds(10,100,30,5);
+        acquiredData.setBorder(new EmptyBorder(0,0,0,0));
+        acquiredData.setHorizontalAlignment(JTextField.CENTER);
 
         playersGroup.add(twoPlayers);
         playersGroup.add(customizedPlayers);
@@ -235,8 +283,17 @@ public class MainFrame extends JFrame {
         levelGroup.add(customized);
 
         jp = new JPanel();
-        jp.setLayout(new GridLayout(0,1));
+        jp.setLayout(new GridLayout(0,4));
         jp.add(selectLevel);
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
         jp.add(basic);
         jp.add(intermediate);
         jp.add(advanced);
@@ -247,11 +304,38 @@ public class MainFrame extends JFrame {
         jp.add(customizedY);
         jp.add(promtMineCount);
         jp.add(customizedMineCount);
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
         jp.add(selectNumberOfPlayers);
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
         jp.add(twoPlayers);
         jp.add(customizedPlayers);
         jp.add(customizedNumberOfPlayers);
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
         jp.add(acquiredData);
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
+        jp.add(new JComponent(){{
+            setBorder(new EmptyBorder(0,0,0,0));
+        }});
 
         basic.addActionListener(listener);
         intermediate.addActionListener(listener);
@@ -260,13 +344,24 @@ public class MainFrame extends JFrame {
         twoPlayers.addActionListener(listener);
         customizedPlayers.addActionListener(listener);
 
-        OK = new JButton("OK");
+        OK = new JButton(OKImage);
+        OK.setContentAreaFilled(false);
+        OK.setOpaque(false);
+        OK.setBorder(new EmptyBorder(0,0,0,0));
         jp.add(OK);
         OK.addActionListener(listener);
-        startButton = new JButton("Start Game!");
+
+        startButton = new JButton(StartImage);
+        startButton.setContentAreaFilled(false);
+        startButton.setOpaque(false);
+        startButton.setBorder(new EmptyBorder(0,0,0,0));
         jp.add(startButton);
         startButton.addActionListener(listener);
-        loadButton = new JButton("Load game...");
+
+        loadButton = new JButton(LoadImage);
+        loadButton.setContentAreaFilled(false);
+        loadButton.setOpaque(false);
+        loadButton.setBorder(new EmptyBorder(0,0,0,0));
         jp.add(loadButton);
         loadButton.addActionListener(listener);
 
